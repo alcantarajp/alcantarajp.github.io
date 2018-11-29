00682717aaebd17d6529e6d6801a5bee87fb1033
João Paulo Alcântara <jpdf1@hotmail.com>
2018-11-29 13:31:21 -0200

    tasdasad

---
+++ index.php
@@ -0,0 +1,18 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Landpage HTML</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript">
+
+    </script>
+</head>
+<body>
+    <h1>Teste de pagina</h1>
+    <?php
+        echo "teste";
+    ?>
+</body>
+</html>
